a5bd07c2c35641b103997043dbda64989069196c
Cindy Yen <cindymeliyen@gmail.com>
2025-07-30 19:44:08 -0700

    add sitemap

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Required for GitHub Pages to serve as a static site
-    output: 'export',
-  
-    // **NO BASE PATH FOR USER PAGES**: Your site is at the root.
-    basePath: '',
-    
-    // Disable default image optimization as it requires a server
-    images: {
-      unoptimized: true,
-    },
-  
-    // Optional: Add a trailing slash to all URLs (can help with some static hosting)
-    trailingSlash: true,
-  };
-  
-  module.exports = nextConfig;
+  // Required for GitHub Pages to serve as a static site
+  output: 'export',
+
+  // **NO BASE PATH FOR USER PAGES**: Your site is at the root.
+  basePath: '',
+
+  // Disable default image optimization as it requires a server
+  images: {
+    unoptimized: true,
+  },
+
+  // Optional: Add a trailing slash to all URLs (can help with some static hosting)
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
